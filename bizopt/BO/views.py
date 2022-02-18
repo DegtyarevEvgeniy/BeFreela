85cf59213426ac118fd78af1d36a506dc10a1d97
Коростылев Дмитрий <koromit15@gmail.com>
2022-02-18 18:17:56 +0300

    fix login

--- bizopt/BO/views.py
+++ bizopt/BO/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+
+
 def index_page(request):
     content = {}
     return render(request, 'index.html', content)
 
+
 def signin_page(request):
     content = {}
     return render(request, 'signin.html', content)
+
 
 def login_page(request):
     content = {}
