11e3b63fcfff9ebce4b6bfe07bd861d5002ef44a
Коростылев Дмитрий <koromit15@gmail.com>
2022-02-20 16:18:07 +0300

    end week commit

--- bizopt/BO/forms.py
+++ bizopt/BO/forms.py
@@ -1,9 +1,11 @@
-class UserForm(forms.Form):
-    login = forms.CharField(label='login', required=True)
-    password = forms.CharField(label='password1', required=True)
+from django import forms
 
-    phone = forms.Integer(max_length=20)
-    name = forms.CharField(max_length=20)
-    surname = forms.CharField(max_length=30)
-    gender = forms.CharField(max_length=20)
-    city = forms.CharField(max_length=20)
+
+class UserForm(forms.Form):
+    login = forms.CharField(label='login', required=True, help_text="Логин")
+    password = forms.CharField(label='password1', required=True, help_text="Пароль")
+    sug_password = forms.CharField(label='password2', required=True, help_text="Подтвердите пароль")
+    phone = forms.Integer(max_length=20, help_text="Телефон")
+    name = forms.CharField(max_length=20, help_text="Имя")
+    surname = forms.CharField(max_length=30, help_text="Фамилия")
+    city = forms.CharField(max_length=20, help_text="Город")
