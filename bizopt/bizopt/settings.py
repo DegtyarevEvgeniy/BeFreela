94590b1ddfa81eb35b8214f84527944a1f8af52e
Yana <2760939@gmail.com>
2022-02-26 22:18:41 +0300

    logout

--- bizopt/bizopt/settings.py
+++ bizopt/bizopt/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGOUT_REDIRECT_URL = '/logged_out/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
