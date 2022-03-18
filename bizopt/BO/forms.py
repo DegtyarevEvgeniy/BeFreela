2e2ae91fb79a0a6c90aeee9292be151285e766a6
Коростылев Дмитрий <koromit15@gmail.com>
2022-03-18 22:54:22 +0300

    signin fix

--- bizopt/BO/forms.py
+++ bizopt/BO/forms.py
@@ -5,18 +5,18 @@ class UserRegistrationForm(forms.Form):
     username = forms.CharField(label='Логин', required=True,
                                widget=forms.TextInput(attrs={'placeholder': 'Логин'}))
     password = forms.CharField(label='Пароль', required=True,
-                               widget=forms.PasswordInput)
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}))
     password2 = forms.CharField(label='Повторите пароль', required=True,
-                                widget=forms.PasswordInput)
-    phone = forms.IntegerField(label='Телефон', widget=forms.TextInput(attrs={'placeholder': 'Телефон'}))
+                                widget=forms.PasswordInput(attrs={'placeholder': 'Подтвердите пароль'}))
+    # phone = forms.IntegerField(label='Телефон', widget=forms.TextInput(attrs={'placeholder': 'Телефон'}))
     name = forms.CharField(label='Имя', max_length=20,
                            widget=forms.TextInput(attrs={'placeholder': 'Имя'}))
     surname = forms.CharField(label='Фамилия', max_length=30,
                               widget=forms.TextInput(attrs={'placeholder': 'Фамилия'}))
-    city = forms.CharField(label='Город', max_length=20,
-                           widget=forms.TextInput(attrs={'placeholder': 'Город'}))
-    email = forms.CharField(label='Почта', max_length=20,
-                            widget=forms.TextInput(attrs={'placeholder': 'Почта'}))
+    # city = forms.CharField(label='Город', max_length=20,
+    #                        widget=forms.TextInput(attrs={'placeholder': 'Город'}))
+    # email = forms.CharField(label='Почта', max_length=20,
+    #                         widget=forms.TextInput(attrs={'placeholder': 'Почта'}))
 
     def clean_password2(self):
         cd = self.cleaned_data
