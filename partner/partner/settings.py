f3c5251a7bb9332fcf055c1c915a1e79123acabb
evgin <evgin@pop-os.localdomain>
2022-07-12 01:16:16 +0300

    add online database witj out migrations

--- partner/partner/settings.py
+++ partner/partner/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'partner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'degtyarev_bf',
+        'USER': 'degtyarev_bf',
+        'PASSWORD': 'DEa.63636363',
+        'HOST': 'degtyarev.beget.tech',
+        'PORT': '3306',
     }
 }
 
