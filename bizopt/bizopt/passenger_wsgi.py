dcf1691fd871583bc31240117e5fe8bf609e651a
GUFFYch <koromit15@gmail.com>
2022-10-10 14:50:12 +0300

    daily commit

--- bizopt/bizopt/passenger_wsgi.py
+++ bizopt/bizopt/passenger_wsgi.py
@@ -4,7 +4,11 @@ import os, sys
 from django.core.wsgi import get_wsgi_application
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 sys.path.insert(0, '/home/d/degtyarev/befreela/public_html/BeFreela/bizopt')
+=======
+sys.path.insert(0, '/home/d/degtyarev/befreela/public_html/bizopt')
+>>>>>>> 0b07ab1 (optimised for beget)
 =======
 sys.path.insert(0, '/home/d/degtyarev/befreela/public_html/bizopt')
 >>>>>>> 0b07ab1 (optimised for beget)
