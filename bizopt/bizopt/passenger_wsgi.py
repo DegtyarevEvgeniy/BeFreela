cc8cd84a71eb8aa12ff7d0d2808f9e2eca84e6fe
GUFFYch <koromit15@gmail.com>
2022-10-10 14:40:50 +0300

    daily commit

--- bizopt/bizopt/passenger_wsgi.py
+++ bizopt/bizopt/passenger_wsgi.py
@@ -3,7 +3,11 @@
 import os, sys
 from django.core.wsgi import get_wsgi_application
 
+<<<<<<< HEAD
 sys.path.insert(0, '/home/d/degtyarev/befreela/public_html/BeFreela/bizopt')
+=======
+sys.path.insert(0, '/home/d/degtyarev/befreela/public_html/bizopt')
+>>>>>>> 0b07ab1 (optimised for beget)
 sys.path.insert(1, '/home/d/degtyarev/befreela/public_html/venv_django/lib/python3.8/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'bizopt.settings'
 
