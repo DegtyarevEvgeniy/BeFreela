01c385c83d2460403edd1310c559de4a49565f36
GUFFYch <koromit15@gmail.com>
2022-10-29 11:08:30 +0300

    dailly commit

--- bizopt/bizopt/settings.py
+++ bizopt/bizopt/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
         'PASSWORD': '4VX6tC2nSQg0Cpiz',
         'HOST': 'localhost',
     #    'PORT': '3306',
+
     #    'OPTIONS': {
     #        'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
     #        'charset': 'utf8mb4',
